9bdb3dc16e495ee46293dedec20e8410bec6cadc
Utkarsh Purbey <77148512+Utkarshpurbey@users.noreply.github.com>
2022-02-20 12:40:45 +0530

    Time: 8 ms (91.67%), Space: 9 MB (93.64%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
     
-    int targetsun(vector<int> &nums,int n,int sum,int target)
+    int countsub(vector<int>nums,int n,int sum)
     {
-        if(n==0)
-        {
-            if(sum==target)
-                return 1;
-            else
-                return 0;
-        }
-        return targetsun(nums,n-1,sum+nums[n-1],target)+targetsun(nums,n-1,sum-nums[n-1],target);
+        int count=0;
+    int t[n+1][sum+1];
         
+        for(int i=0;i<sum+1;i++)
+            t[0][i]=0;
+        for(int j=0;j<n+1;j++)
+            t[j][0]=1;
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
+                if(nums[i-1]<=j)
+                    t[i][j]=t[i-1][j]+t[i-1][j-nums[i-1]];
+                else
+                    t[i][j]=t[i-1][j];
+            }
+        }
+        return t[n][sum];
     }
-    
     
     
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-       return targetsun(nums,n,0,target);
+        target=abs(target);
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+            
+        }
+        if(sum< target || (sum+target)%2!=0)
+            return 0;
+        int newt=(target+sum)/2;
+        return countsub(nums,n,newt);
         
+                
     }
 };
