1d84cd2233c3162e2455f8125d49b88b93989baa
Utkarsh Purbey <77148512+Utkarshpurbey@users.noreply.github.com>
2022-08-04 20:23:28 +0530

    Time: 13 ms (8.26%), Space: 7.9 MB (42.63%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    int solve(int i,int j,int m ,int n,vector<vector<int>> &arr,vector<vector<int>> &dp){
-        if(i==0 && j==0)
-            return 1;
-        if((i<0 || j<0) || arr[i][j]==1)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int up = solve(i-1,j,m,n,arr,dp);
-        int left = solve(i,j-1,m,n,arr,dp);
-        
-        return dp[i][j]=up + left;
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& arr) {
         int m = arr.size();
         int n = arr[0].size();
         
         vector<vector<int>> dp ( m , vector<int>(n,-1));
-        if(arr[0][0]==1)
-            return 0;
         
-        return solve(m-1,n-1,m,n,arr,dp);
+        for(int i=0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(arr[i][j]==1) dp[i][j]=0;
+                else if(i==0 && j==0) dp[i][j]=1;
+                else
+                {
+                    int up =0 , left =0;
+                    if(i>0) up += dp[i-1][j];
+                    if(j>0) left += dp[i][j-1];
+                    
+                    dp[i][j] = up + left;
+                }
+                
+            }
+        }
+        return dp[m-1][n-1];
         
     }
 };
