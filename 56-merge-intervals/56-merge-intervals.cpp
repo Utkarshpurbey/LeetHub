a891a59500e54b2e4476ae2a9e66172fd7c761df
Utkarsh Purbey <77148512+Utkarshpurbey@users.noreply.github.com>
2022-08-05 12:29:07 +0530

    Time: 56 ms (61.06%), Space: 19 MB (66.10%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& v) {
-           vector<vector<int>> ans;
-        int n=v.size();
-        sort(v.begin(),v.end());
-            for(int i=0;i<n;i++)
-            {
-                if(ans.empty())
-                {
-                    ans.push_back(v[i]);
-                }else
-                {
-                    vector<int>&res=ans.back();
-                    //overlapping 
-                    
-                    if(res[1]>=v[i][0])
-                    {
-                        res[1]=max(res[1],v[i][1]);
-                    }
-                    else
-                    {
-                        ans.push_back(v[i]);
-                    }
-                }
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        int n = arr.size(); 
+         sort(arr.begin(),arr.end());
+         vector<vector<int>> ans;
+         ans.push_back(arr[0]);
+         int j = 0; 
+         for(int i =1;i<n;i++)
+         {
+             if(ans[j][1] >= arr[i][0]){ // Overlap kar raha hai
+             ans[j][1] = max(arr[i][1],ans[j][1]);
             }
-        return ans;
+             else
+             {
+                 ans.push_back(arr[i]);
+                 j++;
+             }
+         }
+         return ans;
+        
     }
 };
