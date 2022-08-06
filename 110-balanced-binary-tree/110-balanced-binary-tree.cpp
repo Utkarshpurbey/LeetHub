36ee232cd13d7147d41087db044088784691a0e7
Utkarsh Purbey <77148512+Utkarshpurbey@users.noreply.github.com>
2022-08-06 22:35:57 +0530

    Time: 7 ms (98.89%), Space: 22.2 MB (5.29%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -9,25 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     
-    int height(TreeNode *root){
-        if(root == NULL)
-            return 0;
-        return max(height(root->left),height(root->right))+1;
-    }
+   pair<bool,int> balanced(TreeNode* root){
+            if(root== NULL)
+            return (make_pair(true,0));
+        
+        pair<bool,int> left = balanced(root->left);
+        pair<bool,int> right = balanced(root->right);
+       
+       pair<bool,int > ans;
+       
+       ans.second = max(left.second,right.second)+1;
+       bool prime = abs(left.second - right.second) <=1 ;
+       ans.first = prime && left.first && right.first;
+       
+       return ans;
+ }
     
     
     
     bool isBalanced(TreeNode* root) {
-        if(root == NULL)
-            return true;
-        bool prime = (abs(height(root->left)-height(root->right))<=1);
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
+        return balanced(root).first;
         
-        return (prime && left && right);
+   
         
     }
 };
