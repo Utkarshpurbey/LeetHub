dc15ab4528eee634a28e7f81da88b2c85d42a261
Utkarsh Purbey <77148512+Utkarshpurbey@users.noreply.github.com>
2022-08-09 23:30:38 +0530

    Time: 24 ms (55.70%), Space: 11.5 MB (65.37%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    vector<int> maza;
+    void dfs(vector<vector<int>>&graph,vector<bool>vis,int i,vector<vector<int>>&res)
+    {
+
+        vis[i]=1;
+          maza.push_back(i);  
+        for(int x:graph[i])
+        {
+
+              if(!vis[x] && x!=graph.size()-1)
+              {
+                dfs(graph,vis,x,res);
+                  
+              }
+              else if(!vis[x] )
+              {
+                   maza.push_back(x);
+                   res.push_back(maza);   
+                  
+              }
+            maza.pop_back();
+        }
+    }
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) 
+     {
+       
+        int n =graph.size();
+        vector<bool> vis(n,0);
+        
+        
+        vector<vector<int>> res;
+        
+        dfs(graph,vis,0,res);
+        
+        return res;
+    }
+};
