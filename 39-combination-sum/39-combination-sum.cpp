2f136e600629e87c999387e593b99637330a1954
Utkarsh Purbey <77148512+Utkarshpurbey@users.noreply.github.com>
2022-08-24 07:56:34 +0530

    Time: 4 ms (94.13%), Space: 11.1 MB (89.52%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     void solve(int i,vector<int> &arr,vector<vector<int>>&ans,vector<int> &ds,int target){
-        if(target == 0){
-            ans.push_back(ds);   
-            return;
-        }
-        if(i>=arr.size())
-            return;
+       if(i == arr.size()){
+           if(target == 0){
+               ans.push_back(ds);
+           }
+           return;
+       }
         if(target>=arr[i]){
             ds.push_back(arr[i]);
             solve(i,arr,ans,ds,target-arr[i]);
