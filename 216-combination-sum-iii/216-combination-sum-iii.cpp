39a7454bccf1edf24eecb3d9a5d18f75767ac1f7
Utkarsh Purbey <77148512+Utkarshpurbey@users.noreply.github.com>
2022-09-02 20:11:11 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (40.75%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    void solve(int i,vector<vector<int>>&ans,vector<int> &ds, int k,int n)
-    {
-        if(k==0){
-            if(n==0)
+    
+    void help(int i,int k,int target,vector<int> &ds,vector<vector<int>> &ans){
+        if(target == 0){
+            if(k==0){
                 ans.push_back(ds);
+            }
             return;
         }
         if(i>9)
             return;
-        // take 
-        ds.push_back(i);
-        solve(i+1,ans,ds,k-1,n-i);
-        ds.pop_back();
+        if(k<0)
+            return;
         
-        solve(i+1,ans,ds,k,n);
-        
+        if(target>= i){
+            ds.push_back(i);
+            help(i+1,k-1,target-i,ds,ans);
+            ds.pop_back();
+        }
+        help(i+1,k,target,ds,ans);
     }
-    
     vector<vector<int>> combinationSum3(int k, int n) {
-        int i =1;
-        vector<vector<int>>ans;
+        vector<vector<int>> ans;
+        
         vector<int>ds;
         
-        // call the helper function
+        //help 
+        help(1,k,n,ds,ans);
         
-        solve(i,ans,ds,k,n);
         return ans;
         
     }
